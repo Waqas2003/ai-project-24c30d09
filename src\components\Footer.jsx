71209d1cc31f9306d362c229ad4e9b3e9db0d2c5
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-11 17:18:17 +0500

    Add src\components\Footer.jsx

---
+++ src\components\Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-6">
+      <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+        <p className="text-sm text-gray-600">Copyright 2025 FIFA World Cup</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
